93693fd3f0a05e51d877a3f51261ffd855d69c64
twistiv <robtwistiv@gmail.com>
2023-07-02 19:42:27 -0400

    added chess pieces and movement/undo-movement functionality

---
+++ ChessEngine.py
@@ -0,0 +1,41 @@
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+        self.whiteToMove = True
+        self.moveLog = []
+
+    def move(self, move):
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.board[move.startRow][move.startCol] = "--"
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
+
+    def undoMove(self):
+        move = self.moveLog.pop()
+        self.board[move.endRow][move.endCol] = move.endSquare
+        self.board[move.startRow][move.startCol] = move.pieceMoved
+        self.whiteToMove = not self.whiteToMove
+
+class Move():
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved =  board[self.startRow][self.startCol]
+        self.endSquare = board[self.endRow][self.endCol]
+
+    #def getNotation(self):
--- main.py
+++ main.py
@@ -1,30 +1,88 @@
 import pygame
-
-pygame.init()
+import ChessEngine
 
 CELL_SIZE = 100
+IMAGES = {}
+LIGHT_COLOR = (255, 255, 255)
+DARK_COLOR = (102, 153, 153)
 
-window = pygame.display.set_mode((CELL_SIZE*8, CELL_SIZE*8))
-pygame.display.set_caption('5000 ELO Chess AI')
+'''
+Initialize images
+'''
+def loadImages():
+    pieces = ["bP", "bQ", "bK", "bB", "bN", "bR", "wP", "wQ", "wK", "wB", "wN", "wR"]
+    for piece in pieces:
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load("res/" + piece + ".png"), (CELL_SIZE, CELL_SIZE))
 
-icon = pygame.image.load("res/icon.png")
-pygame.display.set_icon(icon)
 
-run = True
+'''
+Main method, initialize settings and contains game loop
+'''
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((CELL_SIZE*8, CELL_SIZE*8))
+    pygame.display.set_caption('5000 ELO Chess AI')
+    icon = pygame.image.load("res/icon.png")
+    pygame.display.set_icon(icon)
 
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        window.fill((255, 255, 255))
+    gameState = ChessEngine.GameState()
+    loadImages()
 
-        isDarkSquare = False
-        for x in range(8):
-            for y in range(8):
-                if isDarkSquare:
-                    pygame.draw.rect(window,(0,0,0), (x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
-                isDarkSquare = not isDarkSquare
-            isDarkSquare = not isDarkSquare
+    run = True
+
+    selectedSquare = ()
+    moveStartEnd = []
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                click = pygame.mouse.get_pos()
+                col = click[0]//CELL_SIZE
+                row = click[1]//CELL_SIZE
+
+                if selectedSquare == (row, col):
+                    selectedSquare = ()
+                    moveStartEnd = []
+                else:
+                    selectedSquare = (row, col)
+                    moveStartEnd.append(selectedSquare)
+                if len(moveStartEnd) == 2:
+                    move = ChessEngine.Move(moveStartEnd[0], moveStartEnd[1], gameState.board)
+                    gameState.move(move)
+                    selectedSquare = ()
+                    moveStartEnd = []
+
+                #INPUT
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_z:
+                    gameState.undoMove()
+
+        draw(window, gameState)
         pygame.display.update()
 
-pygame.quit()
+    pygame.quit()
+
+def draw(window, gameState):
+    drawBoard(window)
+    drawPieces(window, gameState.board)
+
+def drawBoard(window):
+    window.fill(LIGHT_COLOR)
+
+    isDarkSquare = False
+    for x in range(8):
+        for y in range(8):
+            if isDarkSquare:
+                pygame.draw.rect(window, DARK_COLOR, pygame.Rect(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            isDarkSquare = not isDarkSquare
+        isDarkSquare = not isDarkSquare
+
+def drawPieces(window, board):
+    for row in range(8):
+        for col in range(8):
+            piece = board[row][col]
+            if piece != "--":
+                window.blit(IMAGES[piece], pygame.Rect(col*CELL_SIZE, row*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+main()
